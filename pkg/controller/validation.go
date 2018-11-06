71a18ba7ea48f15b2a46a45e45d28469520c74d5
xuzhonghu <xuzhonghu@huawei.com>
2018-11-06 20:18:13 +0800

    clean code

--- pkg/controller/validation.go
+++ pkg/controller/validation.go
@@ -18,18 +18,10 @@ package controller
 
 import (
 	"fmt"
-	"regexp"
-
 	"k8s.io/apimachinery/pkg/util/validation"
 
 	genev1alpha1 "kubegene.io/kubegene/pkg/apis/gene/v1alpha1"
 )
-
-const dns1123LabelFmt string = "[a-z0-9]([-a-z0-9]*[a-z0-9])?"
-const dns1123LabelErrMsg string = "a DNS-1123 label must consist of lower case alphanumeric characters or '-', and must start and end with an alphanumeric character"
-const dns1123LabelMaxLength int = 63
-
-var dns1123LabelRegexp = regexp.MustCompile("^" + dns1123LabelFmt + "$")
 
 // ValidateExecution accepts a execution and performs validation against it. If lint is specified as
 // true, will skip some validations which is permissible during linting but not submission
